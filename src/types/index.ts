81dbfcb709e674677f240d0f5a47e791e6ced51e
xeniumhs <thexenium07@gmail.com>
2026-09-02 20:34:19 +0545

    data type defined

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,33 @@
+import { IconType } from "react-icons";
+
 export interface Skill {
   name: string;
   category: string;
   description: string;
   icon: string;
 }
-export interface Project {
+
+export type Project = {
   title: string;
   description: string;
   technologies: string[];
+  images: string[];
+  placeholderIcon: IconType;
+
   github?: string;
   demo?: string;
-}
+  youtube?: string;
+
+  layout: "wide" | "normal";
+
+  status?: "completed" | "working";
+  featured?: boolean;
+};
+
+export type Certificate = {
+  title: string;
+  issuer: string;
+  date: string;
+  icon: React.ElementType;
+  certificateUrl: string;
+};
